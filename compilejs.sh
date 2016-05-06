03fc58023febf18c4056989375c78c13e6385999
Daniel Lochrie <dclochri@gmail.com>
2016-05-06 09:18:19 -0500

    Updated the builder/bundler to create separate files for the app, and for dependencies.

--- compilejs.sh
+++ compilejs.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
 
-jspm bundle-sfx app/app.js --minify
+# Bundle, minify, and mangle our application.
+time jspm bundle-sfx app/app.js \
+  - [jspm_packages/**/*] \
+  public/js/application.js \
+  --globals "{'angular': 'angular'}" \
+  --minify
+
+# Bundle 3rd-Party Dependencies into one file, but do NOT mangle (they are
+# already minified).
+time jspm bundle-sfx app/app.js \
+  - [app/**/*] \
+  public/js/dependencies.js \
+  --minify \
+  --no-mangle
--- config.js
+++ config.js
@@ -15,6 +15,7 @@ System.config({
 
   map: {
     "angular": "github:angular/bower-angular@1.5.5",
+    "angular-ui-router": "github:angular-ui/angular-ui-router-bower@0.2.18",
     "babel": "npm:babel-core@5.8.38",
     "babel-runtime": "npm:babel-runtime@5.8.38",
     "core-js": "npm:core-js@1.2.6",
